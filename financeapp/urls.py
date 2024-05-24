e014a7d36e45710f591123d0923cf9b7fc1e1b47
Sreenandika <147902211+Sreenandika@users.noreply.github.com>
2024-05-24 14:11:34 +0530

    first commit

---
+++ financeapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/',about, name='about'),
+    path('services/',services, name='services'),
+    path('contact/',contact, name='contact')
+]
